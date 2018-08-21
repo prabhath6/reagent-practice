673cda8fd8d12e41af6a94c271841f8df3762065
prabhath6 <prabhathkiran20@gmail.com>
2018-08-20 23:07:04 -0500

    Completed Fancy fonts

--- src/reagent_practice/core.cljs
+++ src/reagent_practice/core.cljs
@@ -3,7 +3,8 @@
      [reagent.core :as r]
      [reagent-practice.bmi-component :as bc]
      [reagent-practice.time-component :as tc]
-     [reagent-practice.background-color-changer :as bgc]))
+     [reagent-practice.background-color-changer :as bgc]
+     [reagent-practice.fancy-fonts :as ffc]))
 
 ;; -------------------------
 ;; Views
@@ -12,7 +13,8 @@
   [:div [:h2 "Welcome to Reagent"]
    [bc/bmi-component]
    [tc/timer-component]
-   [bgc/bg-color-changer]])
+   [bgc/bg-color-changer]
+   [ffc/fancy-fonts-component]])
 
 ;; -------------------------
 ;; Initialize app
---
+++ src/reagent_practice/fancy_fonts.cljs
@@ -0,0 +1,25 @@
+(ns reagent-practice.fancy-fonts
+  (:require [reagent.core :as r]))
+
+;; state
+(defonce greeting (r/atom nil))
+
+;; helpers
+
+;; event handlers
+(defn text-handler []
+  [:input {
+           :type "text"
+           :value @greeting
+           :on-change (fn [e]
+                        (reset! greeting (.. e -target -value)))}])
+
+;; component
+(defn fancy-fonts-component []
+  (let [greet @greeting
+        font-families ["Comic Sans MS" "Helvetica" "Courier New"]]
+  [:div
+   [:h3 "Fancy Fonts"]
+   [text-handler]
+   (for [f font-families]
+     [:p {:style {:font-family f :size "18"}} f ": " greet])]))
